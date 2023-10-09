edbe53a49daf87d9642bf7403b2a8843b4d0ac58
kane <mamad2k18@gmail.com>
2023-10-09 22:55:32 +0200

    quelques correction à apporter pour la page services

--- frontend/src/Components/Services/Services.jsx
+++ frontend/src/Components/Services/Services.jsx
@@ -1,8 +1,52 @@
+import { cardContent } from ".";
+import arrowRight from "../../assets/right_arrow.svg";
+import arrowDawn from "../../assets/arrowDawn.svg";
+import { useState, useEffect } from "react";
+
 const Services = () => {
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+  const [fill, setFill] = useState(false);
+
+  useEffect(() => {
+    window.onresize = () => {
+      setWindowWidth(window.innerWidth);
+    };
+  }, [windowWidth]);
+
+  const cards = cardContent.map((card) => {
+    return windowWidth > 775 ? (
+      <div className="card" key={card.id}>
+        <img
+          src={card.image}
+          alt={`${card.title} image`}
+          className="card__logo"
+        />
+        <h2 className="cardTitle">{card.title}</h2>
+        <p className="cardText">{card.text}</p>
+        <p className="cardCta">En savoir plus</p>
+      </div>
+    ) : (
+      <div className="card responsive" key={card.id}>
+        <h2 className="cardTitle">{card.title}</h2>
+        <img
+          src={fill ? arrowDawn : arrowRight}
+          alt="image flèche directionnelle"
+          onClick={() => setFill(!fill)}
+          className="cardArrow"
+        />
+        {fill && (
+          <>
+            <p className="cardText">{card.text}</p>
+            <p className="cardCta">En savoir plus</p>
+          </>
+        )}
+      </div>
+    );
+  });
   return (
     <section className="services">
       <h1 className="services__title">decouvrez nos services</h1>
-      <div className="service__card"></div>
+      <div className="cardSection">{cards}</div>
     </section>
   );
 };
